86e4337fe1b9512c6353ff1f3dd017e3a88ed8c0
Melissa Valim <mellvalim@gmail.com>
2025-04-08 16:49:57 -0300

    v2 (com gráficos)

--- Homework 4/Random walk.py
+++ Homework 4/Random walk.py
@@ -1,18 +1,31 @@
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 def randomwalk(n):
-    x = 0
-    start = x
-    xposition = [start]
-    probabilities = [-1, 1]
-    for i in range(1, n + 1):
-        x += random.choice(probabilities)
-        xposition.append(x)
     
-    return xposition
+    x = [0]
+    y = [0]
+    a = 0.6
+    b = -0.6
+
+    for i in range(1, n + 1):
+        step = a+(b-a)*np.random.rand()
+        xposition = x[-1]+step
+
+        x.append(xposition)
+        y.append(i)
+
+    plt.plot(y, x, c='magenta')
+    plt.title('O andar do bêbado', fontdict={'fontsize': 15,'fontname': 'Georgia'})
+    plt.xlabel('Número de passos', fontdict={'fontsize': 16,'fontname': 'Georgia'})
+    plt.ylabel('Posição', fontdict={'fontsize': 16,'fontname': 'Georgia'})
+    plt.grid(True)
+    plt.show()
+
+    return x, y
 
 print(randomwalk(1000))
-
+    
     
     
